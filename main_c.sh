3faabe5b1358f670ccb030bc366cf0f0fcad07e2
Uroš Grandovec <uros.grandovec@preskok.si>
2024-04-02 15:12:03 +0200

    fix

--- main_c.sh
+++ main_c.sh
@@ -3,8 +3,8 @@
 #SBATCH -p gpu                    # partition (queue)
 #SBATCH --qos=valhala
 #SBATCH -N 1                      # number of nodes
-#SBATCH -n 10                     # number of cores
-#SBATCH --mem=10G                # memory pool for all cores
+#SBATCH --ntasks-per-node=10      # number of cores
+#SBATCH --mem=10G                 # memory pool for all cores
 #SBATCH -t 1-00:00                # time (D-HH:MM)
 #SBATCH -o slurm.%N.%j.out        # STDOUT
 #SBATCH -e slurm.%N.%j.err        # STDERR
