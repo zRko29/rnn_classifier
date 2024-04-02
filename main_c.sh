5a7b63a78960577220737728960fc363cdfceca1
Uroš Grandovec <uros.grandovec@preskok.si>
2024-04-02 13:45:29 +0200

    fix

--- main_c.sh
+++ main_c.sh
@@ -9,6 +9,6 @@
 #SBATCH -o slurm.%N.%j.out        # STDOUT
 #SBATCH -e slurm.%N.%j.err        # STDERR
 
-source ../rnn_generator_env/bin/activate
+source rnn_generator_env/bin/activate
 
-python optimize.py --optimization_steps 5 --models_per_step 10
+python rnn_classifier/optimize.py --optimization_steps 5 --models_per_step 10
