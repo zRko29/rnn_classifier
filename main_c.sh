2e18e19842b346c262093cdffd7045395aa0287b
Uroš Grandovec <uros.grandovec@preskok.si>
2024-04-02 13:38:13 +0200

    fix

--- main_c.sh
+++ main_c.sh
@@ -11,4 +11,4 @@
 
 source ../rnn_generator_env/bin/activate
 
-python /rnn_classifier/optimize.py --optimization_steps 5 --models_per_step 10
+python optimize.py --optimization_steps 5 --models_per_step 10
